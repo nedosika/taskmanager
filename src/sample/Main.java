8a87243ad27d15a901b80a098a85ba21a5b64044
nedosika <nedosika@mail.ru>
2018-06-25 22:22:52 +0300

    Login Panel

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("TaskManager v1.00a");
+        primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
 
