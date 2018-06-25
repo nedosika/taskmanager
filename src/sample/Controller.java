ee805bc72844e94513258114eadf4cdb25c1f9ba
nedosika <nedosika@mail.ru>
2018-06-26 00:11:25 +0300

    add sign Up

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,49 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    @FXML
+    private TextField txtLogin;
+
+    @FXML
+    private Button btnSignUp;
+
+    @FXML
+    private Button btnEnter;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    public void signUpAction(ActionEvent actionEvent) {
+        btnSignUp.getScene().getWindow().hide();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/sample/signUp.fxml"));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
 }
